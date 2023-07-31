389eb94ef717ea54b2666b0caf72d04dae8a75d2
Pratik Ahirrao <56191901+Pratik-ahirrao@users.noreply.github.com>
2023-07-31 21:46:01 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -10,21 +10,21 @@ class Solution{
     int solve(int i, int j, string pattern, string str, vector<vector<int>> &dp){
         
         
-        if(i < 0 && j < 0) return 1;
-        if(i < 0 && j >= 0) return 0;
+        if(i == 0 && j == 0) return 1;
+        if(i == 0 && j > 0) return 0;
         
-        if(i >= 0 && j < 0){
-            for(int idx = 0;idx <= i;idx++){
-                if(pattern[idx] != '*') return 0;
+        if(i > 0 && j == 0){
+            for(int idx = 1;idx <= i;idx++){
+                if(pattern[idx - 1] != '*') return 0;
             }
             return 1;
         }
         
         if(dp[i][j] != -1) return dp[i][j];
-        if(pattern[i] == str[j] || pattern[i] == '?')
+        if(pattern[i - 1] == str[j - 1] || pattern[i - 1] == '?')
             return dp[i][j] = solve(i - 1, j - 1, pattern, str, dp);
             
-        else if(pattern[i] == '*'){
+        else if(pattern[i - 1] == '*'){
             return dp[i][j] = solve(i - 1, j, pattern,str, dp) | solve(i, j - 1, pattern, str, dp);
         }
         
@@ -35,8 +35,8 @@ class Solution{
     {
         int n = pattern.size();
         int m = str.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n - 1, m - 1, pattern, str, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return solve(n, m , pattern, str, dp);
     }
 };
 

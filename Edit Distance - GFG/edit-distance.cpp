f56a3dc2f23e442700d6e0655aa309ac2526c8af
Pratik Ahirrao <56191901+Pratik-ahirrao@users.noreply.github.com>
2023-07-31 19:45:53 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -25,8 +25,30 @@ class Solution {
         // Code here
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return solve(n, m, s, t, dp);
+       // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        //return solve(n, m, s, t, dp);
+        
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+         
+        for(int i = 0;i <= n;i++) dp[i][0] = i;
+        for(int j = 0;j <= m;j++) dp[0][j] = j;
+        
+        for(int i = 1;i <=n;i++){
+            for(int j = 1;j <=m;j++){
+                if(s[i - 1] == t[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];//solve(i - 1, j - 1, s, t,dp);
+                else{
+                    int replace = 1 + dp[i - 1][j - 1];//solve(i - 1, j - 1 ,s, t, dp);
+                    int insert = 1 + dp[i][j - 1];//solve(i, j - 1, s, t, dp);
+                    int del = 1 + dp[i - 1][j];//solve(i - 1, j, s, t, dp);
+        
+                    dp[i][j] = min({replace, insert, del});
+                }
+               
+            }
+        }
+        
+        return dp[n][m];
     }
 };
 

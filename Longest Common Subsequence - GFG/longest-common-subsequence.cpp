23c250d76e41b4e2d1f1bfe6b78903c3f4f594b3
Pratik Ahirrao <56191901+Pratik-ahirrao@users.noreply.github.com>
2023-08-10 22:54:29 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -26,24 +26,24 @@ class Solution
     int lcs(int n, int m, string s1, string s2)
     {
         // your code here
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return solve(n, m, s1,s2, n, m, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        // return solve(n, m, s1,s2, n, m, dp);
         
-        // vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
         
-        // for(int i = 1;i <= n;i++){
-        //     int ans = 0;
-        //     for(int j = 1;j <= m;j++){
-        //         if(s1[i] == s2[j])
-        //             ans = 1 + dp[i - 1][j - 1];//solve(i-1, j-1, s1,s2,n, m, dp);
-        //         else
-        //             ans = max(dp[i - 1][j], dp[i][j - 1]);//solve(i, j-1, s1,s2, n, m, dp));
+        for(int i = 1;i <= n;i++){
+            int ans = 0;
+            for(int j = 1;j <= m;j++){
+                if(s1[i - 1] == s2[j - 1])
+                    ans = 1 + dp[i - 1][j - 1];//solve(i-1, j-1, s1,s2,n, m, dp);
+                else
+                    ans = max(dp[i - 1][j], dp[i][j - 1]);//solve(i, j-1, s1,s2, n, m, dp));
             
-        //         dp[i][j] = ans;
-        //     }
-        // }
+                dp[i][j] = ans;
+            }
+        }
         
-        // return dp[n][m];
+        return dp[n][m];
     }
 };
 

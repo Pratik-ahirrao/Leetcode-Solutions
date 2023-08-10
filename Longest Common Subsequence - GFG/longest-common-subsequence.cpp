1604ed99b3dfecd04fd09aad8c6e4ee21d529825
Pratik Ahirrao <56191901+Pratik-ahirrao@users.noreply.github.com>
2023-08-10 22:53:52 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -13,10 +13,10 @@ class Solution
     
     int solve(int i, int j, string &s1, string &s2, int n, int m, vector<vector<int>> &dp){
         
-        if(i <0 || j < 0) return 0;
+        if(i ==0 || j == 0) return 0;
         int ans = 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i] == s2[j])
+        if(s1[i - 1] == s2[j - 1])
             ans = 1 + solve(i-1, j-1, s1,s2,n, m, dp);
         else
             ans = max(solve(i-1, j, s1,s2, n, m, dp), solve(i, j-1, s1,s2, n, m, dp));
@@ -26,8 +26,24 @@ class Solution
     int lcs(int n, int m, string s1, string s2)
     {
         // your code here
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n- 1, m -1, s1,s2, n, m, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return solve(n, m, s1,s2, n, m, dp);
+        
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        // for(int i = 1;i <= n;i++){
+        //     int ans = 0;
+        //     for(int j = 1;j <= m;j++){
+        //         if(s1[i] == s2[j])
+        //             ans = 1 + dp[i - 1][j - 1];//solve(i-1, j-1, s1,s2,n, m, dp);
+        //         else
+        //             ans = max(dp[i - 1][j], dp[i][j - 1]);//solve(i, j-1, s1,s2, n, m, dp));
+            
+        //         dp[i][j] = ans;
+        //     }
+        // }
+        
+        // return dp[n][m];
     }
 };
 
